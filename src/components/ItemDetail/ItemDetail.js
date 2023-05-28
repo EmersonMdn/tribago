912cee9649a342233a69c8e6b0ec330d19e474d7
Emerson <emersonmedina42@gmail.com>
2023-05-27 21:07:42 -0300

    reservation page on work

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -3,12 +3,16 @@ import "./ItemDetail.css";
 import ItemDetailMain from "../ItemDetailMain/ItemDetailMain";
 import { Order } from "../../contex/OrdersContex";
 import { useContext } from "react";
+import { Helmet } from "react-helmet";
 
 export default function ItemDetail({ item }) {
   const { addOrder, quantity } = useContext(Order);
 
   return (
     <>
+      <Helmet>
+        <title>Windbnb | </title>
+      </Helmet>
       <div className="topbanner">
         <p>
           <i className="fa-solid fa-location-dot"></i> Location: {item.location}
@@ -30,6 +34,10 @@ export default function ItemDetail({ item }) {
         </div>
 
         <div className="item-detail-details">
+          <span className="detail-rate">
+            <i className="fa-solid fa-star"></i> Rate: {item.stars} -{" "}
+            <small> 14 reviews</small>
+          </span>
           <h2 className="detail-description"> {item.description} </h2>
           <p className="details-icons">
             <i className="fa-solid fa-wifi"></i>
@@ -38,22 +46,15 @@ export default function ItemDetail({ item }) {
             <i className="fa-solid fa-mug-hot"></i>
             <i className="fa-solid fa-utensils"></i>
           </p>
-          <p className="detail-type">{item.type}, 1 night </p>
-          {/* <p className="detail-beds">Beds: {item.beds} </p> */}
-
-          <span className="detail-rate">
-            <i className="fa-solid fa-star"></i> Rate: {item.stars} -{" "}
-            <small> 14 reviews</small>
-          </span>
-
           <p className="detail-price">${item.price} </p>
           <small>Taxes included</small>
+
           <div className="buttons">
             <Link
               to="/reservationpage/"
               className="btn btn-order"
               onClick={() => {
-                addOrder(item, 1);
+                addOrder(item, quantity);
               }}
             >
               <i className="fa-solid fa-tags"></i> Reserve
@@ -62,6 +63,8 @@ export default function ItemDetail({ item }) {
               <i className="fa-regular fa-circle-left"></i> Back
             </Link>
           </div>
+          <p className="detail-type">{item.type}, 1 night </p>
+          {/* <p className="detail-beds">Beds: {item.beds} </p> */}
         </div>
       </div>
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ItemsListContainer from "../ItemsListContaienr/itemsListContainer";
 import ItemsListSuperHotContainer from "../ItemsListSuperHotContainer/ItemsListSuperHotContainer";
+import { Helmet } from "react-helmet";
 
 export default function Main({ apartments }) {
   return (
     <main>
+      <Helmet>
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="./media/devchallenges.png"
+        />
+        <title>Windbnb | Home</title>
+      </Helmet>
       <div className="topbar-main">
         <h2>Take a look to ours floors</h2>
         <p>12+ stays</p>
       </div>
 
-      <div className="super-hot-container" id='carousel-container'>
+      <div className="super-hot-container" id="carousel-container">
         <ItemsListSuperHotContainer apartments={apartments} />
       </div>
       <ItemsListContainer apartments={apartments} />
